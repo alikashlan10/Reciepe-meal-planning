1ab96b61c88d8686d0e9dde034488c05e34208eb
alikashlan10 <76254631+alikashlan10@users.noreply.github.com>
2024-04-19 02:59:09 +0200

    handling login & register

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 //controller functions
-const{getUser,userRegister, getAllUsers}= require('../controllers/userController')
+const{getUser,userRegister, getAllUsers,userLogin}= require('../controllers/userController')
 
 const router = express.Router()
 
 //register route
 router.post('/register' , userRegister)
+//login route
+router.post('/login', userLogin)
 //get all users route
 router.get('/', getAllUsers)
 //get a specific user
