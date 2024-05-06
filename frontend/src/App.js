183bc9f9c345c6d89ed94162f4ef10694f0dad63
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2024-05-06 12:56:59 +0300

    add navbar and handling initial state for the authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Switch, BrowserRouter, Routes } from 'r
 //import pages & comoponents
 import RegisterPage from './pages/Register';
 import LoginPage from './pages/login';
+import Navbar from './components/NavBar';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <div className="pages">
           <Routes>
             <Route
