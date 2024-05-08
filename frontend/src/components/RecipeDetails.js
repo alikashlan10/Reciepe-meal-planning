f5be579a3f7cf3762b89eede1b2b6f3277bd51db
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2024-05-08 12:52:21 +0300

    display recipe details and edit styling

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -1,4 +1,3 @@
-// RecipeDetails.js
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,11 +8,68 @@ const RecipeDetails = ({ recipe }) => {
     navigate(`/profile/${username}`);
   };
 
+  const openRecipeWindow = () => {
+    const left = window.screenX + (window.innerWidth - 600) / 2;
+    const top = window.screenY + (window.innerHeight - 400) / 2;
+    const recipeWindow = window.open("", "RecipeWindow", `width=600,height=400,left=${left},top=${top},resizable=no,scrollbars=yes`);
+    recipeWindow.document.write(`
+      <html>
+        <head>
+          <title>${recipe.title} Steps</title>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #fff;
+              padding: 20px;
+              overflow: hidden;
+            }
+            h1 {
+              color: #ff3333;
+            }
+            h2 {
+              color: #000000;
+              background-color:#f1f1f1;
+            }
+            ul, ol {
+              margin-left: 20px;
+            }
+            li {
+              margin-bottom: 5px;
+            }
+          </style>
+          <script>
+            // Disable dragging of the window
+            window.addEventListener('mousedown', function (event) {
+              event.preventDefault();
+            });
+          </script>
+        </head>
+        <body>
+          <h1>${recipe.title} Steps</h1>
+          <h2>Ingredients:</h2>
+          <ul>
+            ${recipe.ingredients.map((ingredient, index) => (
+              `<li key=${index}>${ingredient}</li>`
+            )).join('')}
+          </ul>
+          <h2>Steps:</h2>
+          <ol>
+            ${recipe.steps.map((step, index) => (
+              `<li key=${index}>${step}</li>`
+            )).join('')}
+          </ol>
+        </body>
+      </html>
+    `);
+    recipeWindow.document.close();
+  };
+  
+
   return (
-    <div className="recipe">
+    <div className="recipe" >
       <h2>{recipe.title}</h2>
-      <p>
-        Author:{" "}
+      <h3 className="authorTile">
+        Author  : {" "}
         <span
           className="author"
           onClick={() => handleAuthorClick(recipe.user.username)}
@@ -21,13 +77,14 @@ const RecipeDetails = ({ recipe }) => {
         >
           {recipe.user.username}
         </span>
-      </p>
-      <h3>Ingredients:</h3>
+      </h3>
+      <h3 className="ingredients">Ingredients:</h3>
       <ul>
         {recipe.ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient}</li>
         ))}
       </ul>
+      <h3 className="stepsButton"  onClick={openRecipeWindow} style={{ cursor: "pointer" }}>Steps</h3>
     </div>
   );
 };
